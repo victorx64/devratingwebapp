4d4364f7c368d45ea37e4bcd29f9af419f7afbef
Viktor <viktor_semenov@outlook.com>
2020-02-05 01:41:15 +0900

    #3 Install react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,26 +1,74 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Dev Rating</Link>
+            </li>
+            <li>
+              <Link to="/repositories">Repositories</Link>
+            </li>
+            <li>
+              <Link to="/authors">Authors</Link>
+            </li>
+            <li>
+              <Link to="/works">Works</Link>
+            </li>
+            <li>
+              <Link to="/ratings">Ratings</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/repositories">
+            <Repositories />
+          </Route>
+          <Route path="/authors">
+            <Authors />
+          </Route>
+          <Route path="/works">
+            <Works />
+          </Route>
+          <Route path="/ratings">
+            <Ratings />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
-export default App;
+function Home() {
+  return <h2>Dev Rating</h2>;
+}
+
+function Repositories() {
+  return <h2>Repositories</h2>;
+}
+
+function Authors() {
+  return <h2>Authors</h2>;
+}
+
+function Works() {
+  return <h2>Works</h2>;
+}
+
+function Ratings() {
+  return <h2>Ratings</h2>;
+}
