f20f333b11e163b36a3d45f8f4c780e1a321dda1
Viktor <viktor_semenov@outlook.com>
2020-04-24 08:25:15 +0900

    #27 Add Google Form

--- src/Home.js
+++ src/Home.js
@@ -15,7 +15,7 @@ export default function Home() {
                 Подключи сервис к своему репозиторию и получи рейтинг программистов по редкости исправления их кода после написания.
             </p>
             <p className="row justify-content-center">
-                <a className="btn btn-primary" href="https://github.com/marketplace/dev-rating">Установить Dev Rating</a>
+                <a className="btn btn-primary" href="#form">Получить рейтинг своей команды</a>
             </p>
             <br />
             <h2>
@@ -24,7 +24,7 @@ export default function Home() {
             <p className="lead font-weight-normal">
                 Рейтинг показывает, кто из программистов больше экономит время, путем написания кода с наименьшим числом дефектов.
                 Примените данный сервис для мотивации программистов сокращать время разработки ПО.
-                Чем раньше исправляется дефект – <a href="https://tproger.ru/translations/ten-reasons-why-you-fix-bugs-as-soon-as-you-find-them/">тем дешевле</a> это обходится. 
+                Чем раньше исправляется дефект – <a href="https://tproger.ru/translations/ten-reasons-why-you-fix-bugs-as-soon-as-you-find-them/">тем дешевле</a> это обходится.
                 {/* <a href="https://deepsource.io/blog/exponential-cost-of-fixing-bugs/"></a> */}
                 {/* https://www.ministryoftesting.com/dojo/lessons/ten-reasons-why-you-fix-bugs-as-soon-as-you-find-them */}
             </p>
@@ -42,6 +42,20 @@ export default function Home() {
                 (см. Стив Макконнелл, Совершенный код, второе издание, глава 20.5)
                 и <a href="https://ru.wikipedia.org/wiki/%D0%9F%D1%80%D0%B8%D0%BD%D1%86%D0%B8%D0%BF_%D0%BE%D1%82%D0%BA%D1%80%D1%8B%D1%82%D0%BE%D1%81%D1%82%D0%B8/%D0%B7%D0%B0%D0%BA%D1%80%D1%8B%D1%82%D0%BE%D1%81%D1%82%D0%B8">принцип открытости/закрытости</a>,
                 в частности. <a href="https://github.com/victorx64/devrating#how-it-works">Узнать больше о том, как строится рейтинг...</a>
+            </p>
+            <br />
+            <h2>
+                Чем это лучше Code Churn?
+            </h2>
+            <p className="lead font-weight-normal">
+                Так же, как и рейтинг, Code Churn строится на основе изменений строк кода. Это популярная и наиболее современная метрика в области измерения продуктивности программистов.
+                Проблема Code Churn заключается в том, что его сложно интерпретировать. Он не дает однозначного ответа, что делать в том, или ином случае.
+                Значение этой метрики зависит от большого числа факторов. В <a href="https://www.pluralsight.com/blog/teams/6-causes-of-code-churn-and-what-you-should-do-about-them">статье от Pluralsight</a> приводится
+                шесть различных причин высокого Code Churn: неоднозначные требования, безучастный заказчик, сложная проблема, прототипирование, полировка и низкая вовлеченность программиста.
+                Также, Code Churn меняется со временем в зависимости от зрелости проекта. Часто начинается с высокого значения и постепенно падает по мере стабилизации проекта.
+            </p>
+            <p className="lead font-weight-normal">
+                Dev Rating понятен и имеет однозначное толкование. Рейтинг не зависит от продолжительности проекта и всегда показывает относительную эффективность программистов в момент времени.
             </p>
             <br />
             <h2>
@@ -64,17 +78,16 @@ export default function Home() {
                 подсчета строк кода.
             </p>
             <br /> */}
-
             <h2>
                 Как это помогает распределенным командам?
             </h2>
             <p className="lead font-weight-normal">
                 Сервис делает более прозрачным отслеживание прогресса удаленных программистов.
-                Чем быстрее работает программист по сравнению с другими членами команды, тем выше будет его рейтинг.
+                Чем быстрее и качественнее работает программист по сравнению с другими членами команды, тем выше будет его рейтинг.
             </p>
             <br />
-            <p className="row justify-content-center">
-                <a className="btn btn-primary" href="https://github.com/marketplace/dev-rating">Установить Dev Rating</a>
+            <p className="row justify-content-center" id="form">
+                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdxhJ2olChHV-o0UAdAKo9l0h6jsoF6oE0btSpvhwLbpWfpZw/viewform?embedded=true" width="640" height="1405" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
             </p>
             <br />
         </>
