305e63409eecec20df1b641fbc572761e227e8e8
Viktor <viktor_semenov@outlook.com>
2020-04-30 16:54:23 +0900

    #39 Update text

--- src/Home.js
+++ src/Home.js
@@ -11,8 +11,8 @@ export default function Home() {
                 Developers Rating
             </h1>
             <p className="lead font-weight-normal">
-                Every fix of code is a time, wasted on finding a bug, fixing a bug, and fix verification.
-                Connect our service to your repository and get the rating of your developers by the stability of their code.
+                Each code fix is ​​the time wasted on finding the defect, fixing it, and retesting.
+                Connect the service to your repository and get a rating of programmers by the rarity of fixing their code.
             </p>
             <p className="row justify-content-center">
                 <a className="btn btn-primary" href="#form">Get the rating of your team</a>
@@ -22,9 +22,9 @@ export default function Home() {
                 How the rating system can help you?
             </h2>
             <p className="lead font-weight-normal">
-                The rating shows which developer reduces the development time more by producing code with fewer defects.
-                Use the service to motivate developers to cut the software production time.
-                The sooner you fix a bug – <a href="https://www.ministryoftesting.com/dojo/lessons/ten-reasons-why-you-fix-bugs-as-soon-as-you-find-them">the cheaper</a> it costs.
+                The rating shows which of the programmers saves more time by writing code with the least number of defects.
+                Use this service to motivate programmers to reduce software development time.
+                The sooner the defect is fixed, <a href="https://www.ministryoftesting.com/dojo/lessons/ten-reasons-why-you-fix-bugs-as-soon-as-you-find-them">the cheaper</a> it will be.
                 {/* <a href="https://deepsource.io/blog/exponential-cost-of-fixing-bugs/"></a> */}
             </p>
             <br />
@@ -34,12 +34,12 @@ export default function Home() {
                 How do we build the rating?
             </h2>
             <p className="lead font-weight-normal">
-                The rating is based on the history of lines of code deletions.
-                Every modification of the other developer's code increases your rating points and reduces the rating points of the author of the deleted line of code.
-                To increase rating points a developer has to produce code with a low amount of defects and continuously improve the overall codebase.
-                The rating shows how accurately developers follow The General Principle of Software Quality (Steve McConnell, Code Complete, 2nd Edition, Chapter 20.5)
-                and <a href="https://en.wikipedia.org/wiki/Open%E2%80%93closed_principle">the open–closed principle</a> in
-                particular. <a href="https://github.com/victorx64/devrating#how-it-works">Learn more about the rating...</a>
+                The rating is based on the history of code line deletions.
+                Each correction of someone else's code increases the rating of the programmer and reduces the rating of the author of the corrected code.
+                To increase his rating, the programmer needs to write code without defects and constantly improve the rest of the code.
+                The rating shows how much the programmer follows the General Principle of Software Quality 
+                (Steve McConnell, Code Complete, 2nd Edition, Chapter 20.5) and <a href="https://en.wikipedia.org/wiki/Open%E2%80%93closed_principle">the open–closed principle</a>.
+                Learn more about <a href="https://github.com/victorx64/devrating#how-it-works">how ranking is built...</a>
             </p>
             <br />
             <h2>
@@ -48,8 +48,8 @@ export default function Home() {
             <p className="lead font-weight-normal">
                 As well as the rating, Code Churn is based on the lines of code changes. It's a popular and one of the modern metrics of engineering team productivity.
                 The problem of Code Churn that it is difficult to interpret it univocally. The value of the metric depends on a lot of factors.
-                <a href="https://www.pluralsight.com/blog/teams/6-causes-of-code-churn-and-what-you-should-do-about-them">Pluralsight</a> wrote
-                a blog post about the most common reasons for high Code Churn: unclear requirements, indecisive stakeholder, difficult problem,
+                There is a post in <a href="https://www.pluralsight.com/blog/teams/6-causes-of-code-churn-and-what-you-should-do-about-them">Pluralsight's</a> blog 
+                about the most common reasons for high Code Churn: unclear requirements, indecisive stakeholder, difficult problem,
                 prototyping, polishing, and under-engagement. Also, Code Churn depends on the age of a project. Usually, the value stays
                 high at the beginning of a project and slowly goes down.
             </p>
